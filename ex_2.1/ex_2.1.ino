99d7ba2805cfef5ea0f951b5dca28f9b9080910d
rogerlconflow <rogerlladom@gmail.com>
2016-02-12 20:35:56 +0100

    serial

---
+++ ex_2.1/ex_2.1.ino
@@ -0,0 +1,37 @@
+int a = 10;      // int emmmagatzema un valor de 16 bits, 2 bytes
+int b = 20;
+int c = 30;
+
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+
+  Serial.println("Here is some math: ");
+
+  Serial.print("a = "); //ens escriura a= 
+  Serial.println(a);  // ens escriura el valor de "a" i salta de fila 
+  Serial.print("b = ");
+  Serial.println(b);
+  Serial.print("c = ");
+  Serial.println(c);
+
+  Serial.print("a + b = ");       //suma els valors de a i b 
+  Serial.println(a + b);
+
+  Serial.print("a * c = ");       // multiplica els valors de a i c
+  Serial.println(a * c);
+  
+  Serial.print("c / b = ");       // divideix els valors
+  Serial.println(c / b);
+
+  Serial.print("c % b = ");       //calcula laresta quan es divideixen 2 enters
+  Serial.println(c % b);
+  
+  Serial.print("b - c = ");       // resta els valors 
+  Serial.println(b - c);
+}
+
+void loop()  // we need this to be here even though its empty
+{
+}
+
