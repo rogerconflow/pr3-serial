392204cf1e51329ff8be246d6034054705c162d0
rogerlconflow <rogerlladom@gmail.com>
2016-03-14 23:41:50 +0100

    ex 4.3 / 4.4

--- ex4.3/ex4.3.ino
+++ ex4.3/ex4.3.ino
@@ -3,26 +3,23 @@ int tempAigua = 108;
 void setup()              // run once, when the sketch starts
 {
   Serial.begin(9600);     // set up Serial library at 9600 bps
-if ( tempAigua < 90) 
+if ( tempAigua >100) 
 { 
-  Serial.print ( "Encara no vull"); // quan l'aigua estigui per sota de 100 ens ho escriura 
+  Serial.print ( "Aigua bullint !"); // ens ho executa de dalt a baix, si no es compleix el 1r if, anira baixant 
   } 
  else if (tempAigua >=90 && tempAigua <100 ) 
   {
-  Serial.print( "aigua apunt de bullir");  
+  Serial.print( "aigua apunt de bullir");// else if= si no es cumpleix el 1r if, fara aquest, sino el següent  
   }
-  else if (tempAigua < 100 ) 
-  { 
-    Serial.print ( "Aigua apunt de bullir" ); 
-    } 
-else if ( tempAigua ==100) //quan la temp de l'aigua sigui > o = de 100  ho escriura 
+  else if (tempAigua ==100 )
   {
-    Serial.print("Aigua a 100ºC");
-  }  
- else if (tempAigua >100 )
-  { 
-    Serial.print( "Aigua bullint!"); 
+    Serial.print( "aigua a 100 ºC" );//si n oes cumpleix el else if anterior, provarà aquest 
     }
+  else 
+  { 
+    Serial.print ( "Encara no bull " );// si no es cumpleix cap condició anterior farà això
+    } 
+
 }
 
 void loop()   // we need this to be here even though its empty
---
+++ ex4.4/ex4.4.ino
@@ -0,0 +1,40 @@
+float indicador = 84 ; 
+
+void setup() 
+{ 
+  Serial.begin(9600); 
+  if ( indicador <3.5) 
+  {  // ens escriura A si es mes petit que 3.5 
+    Serial.print ("A"); 
+    } 
+    else if ( indicador < 6.5 && indicador >= 3.5) 
+    { // ens escriura B si es mes petit que 6.5 i mes gran o igual que 3.5
+     Serial.print ("B"); 
+    } 
+    else if ( indicador <11.1 && indicador >=6.5 ) 
+   { // igual amb tots 
+    Serial.print ("C"); 
+   } 
+   else if ( indicador <17.7 && indicador >=11.1 ) 
+   { 
+    Serial.print ("D");  
+    } 
+    else if ( indicador <38.2 && indicador >=17.7 ) 
+   { 
+    Serial.print ("E");  
+    } 
+    else if ( indicador <43.2 && indicador >=38.2 ) 
+   { 
+    Serial.print ("F");  
+    } 
+    else if ( indicador >=43.2 ) 
+   {  // ens escriura G quan sigui més gran o igual que 43.2 
+    Serial.print ("G");  
+    } 
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
